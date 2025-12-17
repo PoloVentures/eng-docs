1b4f8de53dfacfc6d65e19b3230dbd689354612a
GyozaOtoko <ordinalmb@gmail.com>
2025-12-17 14:59:35 -0500

    Add GitHub Actions workflows for deployment and testing

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -22,8 +22,9 @@ const config: Config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'PoloVentures', // Usually your GitHub org/user name.
+  organizationName: 'poloventures', // Usually your GitHub org/user name.
   projectName: 'eng-docs', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
